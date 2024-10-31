d778588ea806f0988227094d774ac0778b5f4e9b
Glicy <Ludovic2008@outlook.com>
2024-10-31 18:03:39 +0100

    squelette
    squelette

---
+++ pages/contact.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../util/util.php'
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/contact.css">
+    <script src="../ts/contact.ts" defer></script>
+    <title>Site personnel</title>
+</head>
+
+<body>
+    <header></header>
+    <main></main>
+</body>
+
+</html>
