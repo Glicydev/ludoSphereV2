9d8c52c7ccfa281c4e77ddaa42d75569afa9a35b
Glicy <Ludovic2008@outlook.com>
2024-11-02 23:13:05 +0100

    button color + scroll less fast
    button color + scroll less fast

--- js/gsap.js
+++ js/gsap.js
@@ -4,7 +4,7 @@ gsap.registerPlugin(ScrollToPlugin)
 
 function scrollToUnder() {
     gsap.to(window, {
-        duration: 1.5,
+        duration: 1.25,
         scrollTo: under,
         ease: "power2"
     })
