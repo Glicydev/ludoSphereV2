b593f5f0fef2c53edaa25242f3d787f3d35493f4
Glicy <Ludovic2008@outlook.com>
2024-11-03 22:50:20 +0100

    languages base
    languages base

--- js/gsap.js
+++ js/gsap.js
@@ -1,16 +1,18 @@
 // Initialisation
 const under = document.getElementById("under");
 const card = document.querySelector(".card");
+const languages = document.querySelectorAll(".languages article");
 
 // PLuggins registering
 gsap.registerPlugin(ScrollToPlugin)
+gsap.registerPlugin(ScrollTrigger)
 
 
 // Scrolling
 function scrollToUnder() {
     gsap.to(window, {
         duration: 1.25,
-        scrollTo: {y: under.offsetTop + 100},
+        scrollTo: { y: under.offsetTop + 100 },
         ease: "power2"
     })
 }
@@ -20,3 +22,20 @@ gsap.from(card, {
     y: 20,
     ease: "elastic.out(1, 0.3)"
 })
+
+languages.forEach(language => {
+    gsap.fromTo(language, {
+        opacity: 0,
+        y: 100
+    }, {
+        opacity: 1,
+        duration: 0.5,
+        y: 0,
+        scrollTrigger: {
+            trigger: language,
+            markers: true,
+            start: "top 70%",
+            toggleActions: "play none none reverse"
+        }
+    });
+});
