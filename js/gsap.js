d8902f1f9bb696695d8fc84248245e8104c18112
Glicy <Ludovic2008@outlook.com>
2024-11-03 13:40:48 +0100

    mini anim
    mini anim

--- js/gsap.js
+++ js/gsap.js
@@ -1,11 +1,22 @@
+// Initialisation
 const under = document.getElementById("under");
+const card = document.querySelector(".card");
 
+// PLuggins registering
 gsap.registerPlugin(ScrollToPlugin)
 
+
+// Scrolling
 function scrollToUnder() {
     gsap.to(window, {
         duration: 1.25,
-        scrollTo: under,
+        scrollTo: {y: under.offsetTop + 100},
         ease: "power2"
     })
 }
+
+gsap.from(card, {
+    duration: 2,
+    y: 20,
+    ease: "elastic.out(1, 0.3)"
+})
